8a9a2a898811a347da478d3254d422e8ed39ca71
kryptoraptor <kryptoraptorblogcz@gmail.com>
2025-11-03 16:31:29 +0100

    PlantList methods
    Added additional methods to plantlist

--- src/cz/engeto/plants/PlantList.java
+++ src/cz/engeto/plants/PlantList.java
@@ -1,5 +1,10 @@
 package cz.engeto.plants;
 
+import java.io.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeParseException;
 import java.util.*;
 
 public class PlantList {
@@ -41,6 +46,85 @@ public class PlantList {
     public void sortLastWatered()
     {
         plants.sort(Comparator.comparing(Plant::getLastWatered));
+    }
+    public List<Plant> getListToWater()
+    {
+        List<Plant> toWater = new ArrayList();
+        for (Plant plant : plants)
+        {
+            if(plant.getLastWatered().plusDays(plant.getWateringFrequency()).isBefore(LocalDate.now()))
+            {
+                toWater.add(plant);
+            }
+        }
+        return toWater;
+    }
+    public void loadFile(String path, String delimiter)
+    {
+        final int NUMBER_OF_PARTS_ON_LINE = 5;
+        int lineNumber = 0;
+        plants.clear();
+        try (Scanner scanner = new Scanner(new BufferedReader(new FileReader(path)))) {
+            while (scanner.hasNextLine()) {
+                lineNumber++;
+                String line = scanner.nextLine();
+                String[] parts = line.split(delimiter);
+                if (parts.length != NUMBER_OF_PARTS_ON_LINE) {
+                    throw new PlantException(
+                            "Nesprávný počet položek na řádku číslo: " +lineNumber+
+                                    "\n"+ line+
+                                    "\nOčekáváno: "+ NUMBER_OF_PARTS_ON_LINE +
+                                    ", skutečně: " + parts.length);
+                }
+                Plant plant = parseLine(parts, lineNumber, line);
+                addPlant(plant);
+            }
+        } catch (FileNotFoundException e) {
+            throw new PlantException(
+                    "Soubor "+path+" nebyl nalezen!\n"+e.getLocalizedMessage());
+        }
+    }
+    private Plant parseLine(String[] parts, int lineNumber, String line)
+            throws PlantException {
+        try {
+            String name = parts[0];
+            String notes = parts[1];
+            LocalDate planted = LocalDate.parse(parts[4]);
+            LocalDate lastWatered = LocalDate.parse(parts[3]);
+            int wateringFrequency = Integer.parseInt(parts[2]);
+            return new Plant(name,notes,planted,lastWatered,wateringFrequency);
+        } catch (NumberFormatException e) {
+            throw new PlantException(
+                    "Chybný formát čísla na řádku číslo: " + lineNumber +
+                            "\n" + line +
+                            "\n" + e.getLocalizedMessage());
+        } catch (DateTimeParseException e) {
+            throw new PlantException(
+                    "Chybný formát data na řádku číslo: " + lineNumber +
+                            "\n" + line +
+                            "\n" + e.getLocalizedMessage());
+
+        }
+        catch (Exception e) {
+            throw new PlantException(
+                    "Chyba při zpracování řádku číslo: " + lineNumber +
+                            "\n"+ line +
+                            "\n" + e.getLocalizedMessage());
+        }
+    }
+    public void saveContentToFile(String path, String delimiter) throws PlantException
+    {
+        try(PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(path)))){
+            for(Plant plant : plants)
+            {
+                String line = plant.getName() + delimiter + plant.getNotes() + delimiter + plant.getWateringFrequency() + delimiter
+                        + plant.getLastWatered() + plant.getPlanted();
+            }
+        } catch (IOException e) {
+            throw new PlantException(
+                    "Chyba při ukládání do souboru " + path + "!\n" + e.getLocalizedMessage()
+            );
+        }
     }
 
 }
