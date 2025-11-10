f205684dd6c1e7e2b44385ad3d3ed71c82c23898
kryptoraptor <kryptoraptorblogcz@gmail.com>
2025-11-10 16:30:19 +0100

    Main method
    Added code to the main method to test functionality of the objects

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,53 @@
+import cz.engeto.plants.Plant;
+import cz.engeto.plants.PlantException;
+import cz.engeto.plants.PlantList;
+
+import java.time.LocalDate;
+import java.util.Comparator;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        PlantList list = new PlantList();
+        try {
+            list.loadFile("resources/kvetiny.txt","\t");
+        } catch (PlantException e)
+        {
+            list.clearList();
+            System.err.println("Chyba při načítání souboru: " + e.getMessage());
+        }
+        for(Plant plant:list.getListCopy())
+        {
+            System.out.println(plant.getWateringInfo());
+        }
+        list.addPlant(new Plant("Tulipán na prodej 0",14));
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        for (int i = 1; i < 11; i++) {
+            list.addPlant(new Plant("Tulipán na prodej " + i,14));
+        }
+        list.removePlant(2);
+        list.saveContentToFile("resources/kvetiny-output.txt","\t");
+        try {
+            list.loadFile("resources/kvetiny-output.txt","\t");
+        } catch (PlantException e)
+        {
+            list.clearList();
+            System.err.println("Chyba při načítání souboru: " + e.getMessage());
+        }
+        for(Plant plant:list.getListCopy())
+        {
+            System.out.println(plant.getWateringInfo());
+        }
+        list.sortByLastWatered();
+        for(Plant plant:list.getListCopy())
+        {
+            System.out.println(plant.getWateringInfo());
+        }
+        list.sortList(Comparator.comparing(Plant::getPlanted));
+        for(Plant plant:list.getListCopy())
+        {
+            System.out.println(plant.getName() + ", zasazena " + plant.getPlanted());
         }
     }
 }
